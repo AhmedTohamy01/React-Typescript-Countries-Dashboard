dd371ac872c65aa2f18ca6e021a5db6953677464
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-31 23:08:45 +0200

    feat: add GetData custom-hook

---
+++ src/custom-hooks/GetData.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+
+export default GetData
+
+function GetData() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then((fetchedData) => fetchedData.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.log(error))
+  }, [])
+
+  return { data }
+}
